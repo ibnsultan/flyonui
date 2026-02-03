40f17589f945a20646106f2e6fbced95d77f96a1
ibnsultan <abdulbasitsultan4@gmail.com>
2026-02-03 21:43:38 +0300

    feat: add configuration kits own configuration file

---
+++ config/flyonui.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Default Theme
+    |--------------------------------------------------------------------------
+    |
+    | This option controls the default theme for the application.
+    | You can change this to any theme supported by FlyonUI.
+    |
+    */
+
+    'theme' => env('FLYONUI_THEME', 'light'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | Enable disable registration
+    |--------------------------------------------------------------------------
+    |
+    | This option controls whether user registration is enabled or disabled.
+    |
+    */
+
+    'registration_enabled' => env('FLYONUI_REGISTRATION_ENABLED', false),
+
+];
